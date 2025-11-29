07299d9b60b9566f18f5bbd78acc5810181f6c86
Amedeo Quaglieri <amedeoquaglieri@gmail.com>
2025-11-29 17:21:00 +0930

    adding starting project

---
+++ firstprediction.py
@@ -0,0 +1,15 @@
+# https://github.com/OlafenwaMoses/ImageAI
+
+from imageai.Classification import ImageClassification
+import os
+
+execution_path = os.getcwd()
+
+prediction = ImageClassification()
+prediction.setModelTypeAsInceptionV3()
+prediction.setModelPath(os.path.join(execution_path, "inception_v3_google-1a9a5a14.pth"))
+prediction.loadModel()
+
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "giraffe.jpg"), result_count=5)
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
